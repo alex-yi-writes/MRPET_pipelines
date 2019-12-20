4a9a59e359b7eb495fdbb2651ff9026f48c8eda9
alex-yi-writes <57142280+alex-yi-writes@users.noreply.github.com>
2019-12-20 17:22:14 +0100

    Add files via upload

---
+++ CAMCAN_seg.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# setup paths
+path_parent=/Volumes/ALEX_DATA/CAMCAN/
+
+# assign number of CPUs used
+CPUs=3
+
+# read in subjects
+ls -d "${path_parent}"/*/ > "${path_parent}"/subjs.txt
+
+while read folder; do
+
+  ID=$(echo "${folder}" | grep -o -E '[0-9][0-9][0-9][0-9][0-9][0-9]')
+  recon-all \
+  -i  ${path_parent}T1w_clusterIKND/${ID}_T1w.nii.gz \
+  -s  ${ID} \
+  -sd ${folder} \
+  -all
+
+  echo "ID ${ID} DONE!"
+
+done < "${path}"/subjs.txt
