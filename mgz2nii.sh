7386ab5ce12893efe66bdbd8669e2d65f375c292
alex-yi-writes <57142280+alex-yi-writes@users.noreply.github.com>
2020-01-13 09:50:33 +0100

    Add files via upload

---
+++ mgz2nii.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mri_convert --in_type mgz --out_type nii --out_orientation RAS /Users/yeojin/Desktop/E_data/EA_raw/EAB_MRI/EABD_segmented/4001_2/40012/mri/aparc+aseg.mgz /Users/yeojin/Desktop/E_data/EA_raw/EAB_MRI/EABD_segmented/4001_2/40012/mri/aparc+aseg.nii
