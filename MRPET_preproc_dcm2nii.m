0baad2dcbdd2aec5e16f020eca87873c0bbe409c
alex-yi-writes <57142280+alex-yi-writes@users.noreply.github.com>
2020-01-20 15:53:38 +0100

    Add files via upload

--- MRPET_preproc_dcm2nii.m
+++ MRPET_preproc_dcm2nii.m
@@ -37,12 +37,7 @@ days = [0 2];
 % file-specicfic variables: choose whatever you'd like to convert
 extensions    = '*.IMA';
 filetypes_MRI = {'MoCoSeries';'MPRAGE';'Hippocampus';'GRE3D';'t2_tse'};
-filetypes_PET = {'PET-Task-MoCo*T0*_AC_';'PET-Task-MoCo*T300_*_AC_';'PET-Task-MoCo*T600*_AC_';'PET-Task-MoCo*T900*_AC_';...
-    'PET-Task-MoCo*T1200*_AC_';'PET-Task-MoCo*T1500*_AC_';'PET-Task-MoCo*T1800*_AC_';'PET-Task-MoCo*T2100*_AC_';...
-    'PET-Task-MoCo*T2400*_AC_';'PET-Task-MoCo*T2700*_AC_';'PET-Task-MoCo*T3000_*_AC_';...
-    'PET-InFlow_HD';'PET-Baseline_HD'};
-seriesnum     = numel(~isnan(cell2mat(strfind(filetypes_PET,'Task')))); % see how many binned PET data there are
-binsize       = 300; % how long are the time bins for now?
+filetypes_PET = {'PET-InFlow_HD*_AC_';'PET-Baseline_HD*_AC_'};
 
 %% MRI part
 
@@ -174,7 +169,7 @@ for id = 1:length(IDs)
     
 end
 
-%% PET part
+%% PET part: other
 %% run
 
 % spm fmri
@@ -272,10 +267,140 @@ for id = 1:length(IDs)
                     flist_dicom = flist_dicom_tmp;
                     clear fnames
                     
+                    fname_string = [num2str(IDs(id)) '_PET_4D_' filetypes_PET{imc} num2str(d)];
+                    
+                    % run 3D conversion
+                    [s,w] = unix([paths.dcm2nii... 
+                        ' -f "' fname_string '"'... 
+                        ' -p y -z n -ba n -o "' ... 
+                        paths.dat_4D num2str(IDs(id)) '_' num2str(d) '" ' ... 
+                         '"' paths.raw num2str(IDs(id)) '_' num2str(d) '/' tmpfname.name '/' templist1(1).name '"'])
+                    clear flist_dicom
+                    
+                    fprintf('\n\n 3D CONVERT DONE \n\n')
+                    
+                    
+                    %% move file for preprocessing
+                    cd(paths.MT); mkdir([num2str(IDs(id)) '_' num2str(d)]);
+                    cd([paths.dat_4D num2str(IDs(id)) '_' num2str(d) '/'])
+                    copyfile([fname_string '.nii'],[paths.MT num2str(IDs(id)) '_' num2str(d) '/'])
+                    clear seqname matlabbatch fname_string
+                    
+                end
+            end
+        end
+        
+    end
+    
+end
+
+%% PET part: task
+%% run
+
+filetypes_pTask = {'PET-Task-MoCo*T0*_AC_';'PET-Task-MoCo*T300_*_AC_';'PET-Task-MoCo*T600*_AC_';'PET-Task-MoCo*T900*_AC_';...
+    'PET-Task-MoCo*T1200*_AC_';'PET-Task-MoCo*T1500*_AC_';'PET-Task-MoCo*T1800*_AC_';'PET-Task-MoCo*T2100*_AC_';...
+    'PET-Task-MoCo*T2400*_AC_';'PET-Task-MoCo*T2700*_AC_';'PET-Task-MoCo*T3000_*_AC_'};
+seriesnum     = numel(~isnan(cell2mat(strfind(filetypes_pTask,'Task')))); % see how many binned PET data there are
+binsize       = 300; % how long are the time bins for now?
+
+for id = 1:length(IDs)
+    
+    for imc = 1:length(filetypes_pTask)
+        fprintf('\n %s \n', [filetypes_pTask{imc} num2str((imc-1)*binsize)])
+        
+        for d = 1:2
+            
+            if days(id,d) == 0
+                warning('day %d doesn''t exist for this participant',d)
+            else
+                
+                fprintf('\n ID: %d\n', IDs(id))
+                
+                
+                spm_jobman('initcfg')
+                clear matlabbatch
+                
+                %% dcm to 3D
+                
+                % setup directories and working directories
+                cd(paths.dat_3D); mkdir([num2str(IDs(id)) '_' num2str(d)])
+                cd([paths.raw num2str(IDs(id)) '_' num2str(d)])
+                tmpfname = dir('study*'); cd(tmpfname.name)
+                numiter   = length(dir(['*' filetypes_pTask{imc} '*' ]));
+                
+                if numiter >= 2
+                    
+                    clear fnames
+                    cd(paths.dat_3D); mkdir([num2str(IDs(id)) '_' num2str(d)])
+                    cd([paths.raw num2str(IDs(id)) '_' num2str(d) '/' tmpfname.name])
+                    templist1   = dir(['*' filetypes_pTask{imc} '*' ]);
+                    
+                    for scans = 1:numiter
+                        
+                        clear fnames
+                        path_origin = paths.dat_4D; cd(paths.dat_4D); mkdir([num2str(IDs(id)) '_' num2str(d)])
+                        
+                        cd([paths.raw num2str(IDs(id)) '_' num2str(d) '/' tmpfname.name])
+                        tempdir1    = templist1(scans).name; cd(tempdir1);
+                        fnames = dir('PI*');
+                        
+                        cd([paths.dat_3D num2str(IDs(id)) '_' num2str(d)]); mkdir(templist1(scans).name);
+                        savepath    = [paths.dat_3D num2str(IDs(id)) '_' num2str(d) '/' templist1(scans).name '/'];
+                        
+                        clear flist_dicom flist_dicom_tmp
+                        for i2 = 1:length(fnames) % assemble into a list
+                            flist_dicom_tmp{i2,:} = [ paths.raw num2str(IDs(id)) '_' num2str(d) '/' tmpfname.name '/' templist1(scans).name '/' fnames(i2,1).name ];
+                        end
+                        flist_dicom = flist_dicom_tmp;
+                        
+                        if imc <= seriesnum % maintask
+                            fname_string = [num2str(IDs(id)) '_PET_3D_T' num2str((imc-1)*binsize) '_MT' num2str(d) '_' num2str(scans)];
+                        else
+                            fname_string = [num2str(IDs(id)) '_PET_4D_' filetypes_pTask{imc} num2str(d) '_' num2str(scans)];
+                        end
+                        
+                        % run 3D conversion
+                        [s,w] = unix([paths.dcm2nii... 
+                            ' -f "' fname_string '"'... 
+                            ' -p y -z n -ba n -o "' ...
+                            paths.dat_4D num2str(IDs(id)) '_' num2str(d) '" ' ... 
+                            '"' paths.raw num2str(IDs(id)) '_' num2str(d) '/' tmpfname.name '/' templist1(1).name '"']) 
+                        clear flist_dicom
+                        
+                        fprintf('\n\n 3D CONVERT DONE \n\n')
+                                               
+                        
+                        %% move file for preprocessing
+                        cd(paths.MT); mkdir([num2str(IDs(id)) '_' num2str(d)]);
+                        cd([paths.dat_4D num2str(IDs(id)) '_' num2str(d) '/'])
+                        copyfile([fname_string '.nii'],[paths.MT num2str(IDs(id)) '_' num2str(d) '/'])
+                        clear seqname matlabbatch fname_string
+                        
+                    end
+                elseif numiter < 2
+                    
+                    clear fnames
+                    path_origin = paths.dat_4D; cd(paths.dat_4D); mkdir([num2str(IDs(id)) '_' num2str(d)])
+                    
+                    cd([paths.raw num2str(IDs(id)) '_' num2str(d) '/' tmpfname.name])
+                    templist1   = dir(['*' filetypes_pTask{imc} '*' ]);
+                    tempdir1    = templist1.name; cd(tempdir1);
+                    fnames = dir('PI*');
+                    
+                    cd([paths.dat_3D num2str(IDs(id)) '_' num2str(d)]); mkdir(templist1(1).name);
+                    savepath    = [paths.dat_3D num2str(IDs(id)) '_' num2str(d) '/' templist1(1).name '/'];
+                    
+                    clear flist_dicom flist_dicom_tmp
+                    for i2 = 1:length(fnames) % assemble into a list
+                        flist_dicom_tmp{i2,:} = [ paths.raw num2str(IDs(id)) '_' num2str(d) '/' tmpfname.name '/' templist1(1).name '/' fnames(i2,1).name ];
+                    end
+                    flist_dicom = flist_dicom_tmp;
+                    clear fnames
+                    
                     if imc <= seriesnum % maintask
                         fname_string = [num2str(IDs(id)) '_PET_3D_T' num2str((imc-1)*binsize) '_MT' num2str(d)];
                     else
-                        fname_string = [num2str(IDs(id)) '_PET_4D_' filetypes_PET{imc} num2str(d)];
+                        fname_string = [num2str(IDs(id)) '_PET_4D_' filetypes_pTask{imc} num2str(d)];
                     end
                     
                     % run 3D conversion
@@ -301,7 +426,11 @@ for id = 1:length(IDs)
         
     end
     
-    % now make 4D volume out of PET-task data
+end
+
+%% now make 4D volume out of PET-task data
+
+for id = 1:length(IDs)
     for d = 1:2
         
         if days(id,d) == 0
@@ -310,8 +439,8 @@ for id = 1:length(IDs)
         else
             flist_PET = [];
             for frames = 1:seriesnum
-                flist_PET{frames,1} = [paths.MT num2str(IDs(id)) '_' num2str(d) '/' ...
-                    num2str(IDs(id)) '_PET_3D_T' num2str((seriesnum-1)*binsize) '_MT' num2str(d) '.nii,1'];
+                flist_PET{frames,1} = [paths.dat_4D num2str(IDs(id)) '_' num2str(d) '/' ...
+                    num2str(IDs(id)) '_PET_3D_T' num2str((frames-1)*binsize) '_MT' num2str(d) '.nii,1'];
             end
             
             % run
@@ -325,7 +454,15 @@ for id = 1:length(IDs)
             
             fprintf('\n\n 4D CONVERT DONE \n\n')
             
+            %% move file for preprocessing
+            
+            fname_string = [num2str(IDs(id)) '_PET_4D_MT' num2str(d)];
+            
+            cd(paths.MT); mkdir([num2str(IDs(id)) '_' num2str(d)]);
+            cd([paths.dat_4D num2str(IDs(id)) '_' num2str(d) '/'])
+            copyfile([fname_string '.nii'],[paths.MT num2str(IDs(id)) '_' num2str(d) '/'])
+            clear seqname matlabbatch fname_string
+            
         end
     end
-    
 end
