33a9ce4c735a7e8f036068c25500fd5d9b230751
alex-yi-writes <57142280+alex-yi-writes@users.noreply.github.com>
2020-01-22 11:33:35 +0100

    Add files via upload

--- preproc_functions/preproc_realign_PET.m
+++ preproc_functions/preproc_realign_PET.m
@@ -2,26 +2,23 @@ function [config] = preproc_realign_PET(ID,flist)
 
 spm_jobman('initcfg')
 
-matlabbatch{1}.spm.spatial.realign.estwrite.data = {flist};
-matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.quality = 0.9;
-matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.sep = 4;
-matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.fwhm = 5;
-matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.rtm = 1;
-matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.interp = 2;
-matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.wrap = [0 0 0];
-matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.weight = '';
-matlabbatch{1}.spm.spatial.realign.estwrite.roptions.which = [2 1];
-matlabbatch{1}.spm.spatial.realign.estwrite.roptions.interp = 4;
-matlabbatch{1}.spm.spatial.realign.estwrite.roptions.wrap = [0 0 0];
-matlabbatch{1}.spm.spatial.realign.estwrite.roptions.mask = 1;
-matlabbatch{1}.spm.spatial.realign.estwrite.roptions.prefix = 'r';
+%% estimate only
+matlabbatch{1}.spm.spatial.realign.estimate.data = {flist};
+matlabbatch{1}.spm.spatial.realign.estimate.eoptions.quality = 0.9;
+matlabbatch{1}.spm.spatial.realign.estimate.eoptions.sep = 4;
+matlabbatch{1}.spm.spatial.realign.estimate.eoptions.fwhm = 5;
+matlabbatch{1}.spm.spatial.realign.estimate.eoptions.rtm = 1;
+matlabbatch{1}.spm.spatial.realign.estimate.eoptions.interp = 4;
+matlabbatch{1}.spm.spatial.realign.estimate.eoptions.wrap = [0 0 0];
+matlabbatch{1}.spm.spatial.realign.estimate.eoptions.weight = '';
 
-%%
+%% estimate & write
+% matlabbatch{1}.spm.spatial.realign.estwrite.data = {flist};
 % matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.quality = 0.9;
 % matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.sep = 4;
 % matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.fwhm = 5;
 % matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.rtm = 1;
-% matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.interp = 4;
+% matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.interp = 2;
 % matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.wrap = [0 0 0];
 % matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.weight = '';
 % matlabbatch{1}.spm.spatial.realign.estwrite.roptions.which = [2 1];
